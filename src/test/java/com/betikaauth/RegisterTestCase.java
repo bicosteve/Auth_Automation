a1bd7dd6a09029e058fb434a63dfc964b0d73544
bicosteve <bicosteve4@gmail.com>
2023-01-03 10:34:05 +0300

    Added register test case

--- src/test/java/com/betikaauth/RegisterTestCase.java
+++ src/test/java/com/betikaauth/RegisterTestCase.java
@@ -1,12 +1,39 @@
 package com.betikaauth;
 
 import com.betikaauth.base.BaseTest;
-import com.betikaauth.pages.BasePage;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import com.betikaauth.pages.RegisterPage;
+import com.betikaauth.pages.VerificationPage;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class RegisterTestCase extends BaseTest {
+
+    @Test
+    public void register() throws InterruptedException {
+        RegisterPage registerPage = new RegisterPage(driver);
+
+        registerPage.loadRegisterPage();
+        registerPage.register("0703445566","xyz","xyz");
+        registerPage.clickRegister();
+
+        Thread.sleep(4000);
+
+        Assert.assertEquals(registerPage.confirmRegister(),"Verify and Log In");
+
+//        VerificationPage verificationPage = registerPage.clickRegister();
+
+        VerificationPage verificationPage = new VerificationPage(driver);
+        verificationPage.loadVerificationPage();
+        verificationPage.verifyAccount("0703445566","1234");
+
+        Thread.sleep(3000);
+
+        Assert.assertTrue(verificationPage.confirmVerification());
+    }
+
+
 
 
 
